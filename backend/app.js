907820fab51d23f236b57f2c4686044a4717b3b3
steven <sg.lesaux@gmail.com>
2022-03-13 01:47:30 +0100

    First commit

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const sauceRoutes = require("./routes/sauce");
+const userRoutes = require("./routes/user");
+const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://lost_in_database:findmyway@cluster0.8i8mr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/auth", userRoutes);
+
+module.exports = app;
